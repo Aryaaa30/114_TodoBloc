09fb191e09ce172b50a8379c8369a7e1075171c6
Aryaaa30 <arya.anugrah.hsb@gmail.com>
2025-05-08 17:25:51 +0700

    Membuat Todopage

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:session6_bloc_consept_todo/bloc/bloc.dart';
+import 'package:session6_bloc_consept_todo/ui/todo_page.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
--- lib/ui/todo_page.dart
+++ lib/ui/todo_page.dart
@@ -1,3 +1,15 @@
 import 'package:session6_bloc_consept_todo/ui/todo_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+
+class TodoPage extends StatelessWidget {
+  const TodoPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final _key = GlobalKey<FormState>();
+    final _controller = TextEditingController();
+
+    return Scaffold();
+  }
+}
